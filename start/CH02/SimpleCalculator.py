7df82a10c9c7b5c418f0e08ef4b815e76d4a820d
samzad <Szadneprovskiy@mail.greenriver.edu>
2021-10-03 03:09:00 -0700

    Created Calculator

--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # A simple calculator to show math and conditionals
-# Created 
+# Created by Sam on 10/1/21
+
+# Get information from user
+first_number = float( input("What is the first number? ") )
+activity = input("What activity? ( + - * / ) ")
+second_number = float( input("What is the second number? ") )
+
+# Do math
+if activity == "+":
+    print(first_number + second_number)
+if activity == "-":
+    print(first_number - second_number)
+if activity == "*":
+    print(first_number * second_number)
+if activity == "/":
+    print(first_number / second_number)
