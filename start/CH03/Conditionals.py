c2335a39ef1de14e9f59c5db19cc83b15f805ff4
samzad <Szadneprovskiy@mail.greenriver.edu>
2021-10-11 01:19:15 -0700

    Added second ping

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Sam on 10/10/21
+
+number = int(input("Choose a number between 1 and 4: "))
+
+if number == 1:
+    print("Wrong number, try again")
+    print("Another example")
+elif number == 2:
+    print("Wrong number, try again")
+elif number == 3:
+    print("Correct!")
+else:
+    print("Wrong number, try again")
+
